ae40d8ffd3260d87b0e05ac1869dbb8d37e6d0ac
lijing <1330436958@qq.com>
2016-12-07 14:18:55 +0800

    增加toolbar

--- app/src/main/java/com/sunjiajia/androidnewwidgetsdemo/MovieActivity.java
+++ app/src/main/java/com/sunjiajia/androidnewwidgetsdemo/MovieActivity.java
@@ -22,7 +22,10 @@ package com.sunjiajia.androidnewwidgetsdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -31,5 +34,28 @@ public class MovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
         Log.e("aaaaaa",getIntent().getStringExtra("a"));
+        Toolbar toolbar= (Toolbar) findViewById(R.id.id_toolbar);
+        toolbar.setTitle("视频");
+        setSupportActionBar(toolbar);  // 用ToolBar代替ActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // TODO Auto-generated method stub
+        if(item.getItemId() == R.id.id_toolbar)
+        {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
