2a5f0e1c1397982dce09bbef3e05adb71a7c7a09
lijing <1330436958@qq.com>
2016-12-08 11:22:45 +0800

    添加MyApplication,并配置全局okgo，添加Urls，增加配置文件中的appname

---
+++ app/src/main/java/com/sunjiajia/androidnewwidgetsdemo/MyAplication.java
@@ -0,0 +1,71 @@
+/*
+ *
+ *  *
+ *  *  *
+ *  *  *  * ===================================
+ *  *  *  * Copyright (c) 2016.
+ *  *  *  * 作者：安卓猴
+ *  *  *  * 微博：@安卓猴
+ *  *  *  * 博客：http://sunjiajia.com
+ *  *  *  * Github：https://github.com/opengit
+ *  *  *  *
+ *  *  *  * 注意**：如果您使用或者修改该代码，请务必保留此版权信息。
+ *  *  *  * ===================================
+ *  *  *
+ *  *  *
+ *  *
+ *
+ */
+
+package com.sunjiajia.androidnewwidgetsdemo;
+
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.cookie.store.PersistentCookieStore;
+import com.lzy.okgo.model.HttpHeaders;
+import com.lzy.okgo.model.HttpParams;
+
+import java.util.logging.Level;
+
+/**
+ * Created by lijing on 2016/12/8.
+ */
+
+public class MyAplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        HttpHeaders headers=new HttpHeaders();
+        headers.put("","");
+        headers.put("","");
+        HttpParams params=new HttpParams();
+        params.put("","");
+        OkGo.init(this);
+
+
+        try {
+            OkGo.getInstance()
+                    .debug("OKGo", Level.INFO,true)
+                    .setConnectTimeout(OkGo.DEFAULT_MILLISECONDS)
+                    .setReadTimeOut(OkGo.DEFAULT_MILLISECONDS)
+                    .setWriteTimeOut(OkGo.DEFAULT_MILLISECONDS)
+                    .setCacheMode(CacheMode.NO_CACHE)
+                    .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)
+                    .setRetryCount(3)
+                    .setCookieStore(new PersistentCookieStore())
+                    .setCertificates()
+                    .addCommonHeaders(headers)
+                    .addCommonParams(params);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/sunjiajia/androidnewwidgetsdemo/Urls.java
@@ -0,0 +1,35 @@
+/*
+ *
+ *  *
+ *  *  *
+ *  *  *  * ===================================
+ *  *  *  * Copyright (c) 2016.
+ *  *  *  * 作者：安卓猴
+ *  *  *  * 微博：@安卓猴
+ *  *  *  * 博客：http://sunjiajia.com
+ *  *  *  * Github：https://github.com/opengit
+ *  *  *  *
+ *  *  *  * 注意**：如果您使用或者修改该代码，请务必保留此版权信息。
+ *  *  *  * ===================================
+ *  *  *
+ *  *  *
+ *  *
+ *
+ */
+
+package com.sunjiajia.androidnewwidgetsdemo;
+
+/**
+ * Created by lijing on 2016/12/8.
+ */
+
+public class Urls {
+
+    public final static String SERVER = "http://122.5.13.154/ROP/api";
+    public final static String url = "http://122.5.13.154/ROP/";
+    public final static String QRURL = "http://122.5.13.154/ROP/customercreate";
+    public final static String COPYRIGHT = "http://122.5.13.154/ROP/banquanxinxi";
+    public final static String LICENSE = "http://122.5.13.154/ROP/ruanjianxieyi";
+
+    public final static String zhifubaourl = "http://122.5.13.154/ROP/OnlineRemittance/zfbzhifu";
+}
